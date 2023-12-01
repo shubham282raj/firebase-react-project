9561b67c6215eba3b8b92c8d6a9a87c788a1a512
shubham282raj <shubham282.me@gmail.com>
2023-12-01 13:57:16 +0530

    Added CSS

--- src/pages/create-post/Form.tsx
+++ src/pages/create-post/Form.tsx
@@ -1,6 +1,9 @@
 import { useForm } from "react-hook-form";
 import * as yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
+import { addDoc, collection } from "firebase/firestore";
+import { auth, db } from "../../config/firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
 
 interface CreateFormData {
   title: string;
@@ -8,6 +11,8 @@ interface CreateFormData {
 }
 
 export const CreateForm = () => {
+  const [user] = useAuthState(auth);
+
   const schema = yup.object().shape({
     title: yup.string().required("You must add a title!"),
     description: yup.string().required("You must give a description!"),
@@ -21,17 +26,27 @@ export const CreateForm = () => {
     resolver: yupResolver(schema),
   });
 
-  const onCreatePost = (data: any) => {
-    console.log(data);
+  const postsRef = collection(db, "posts");
+
+  const onCreatePost = async (data: any) => {
+    await addDoc(postsRef, {
+      // title: data.title,
+      // description: data.description,
+      ...data,
+      username: user?.displayName,
+      userId: user?.uid,
+    });
   };
 
   return (
-    <form onSubmit={handleSubmit(onCreatePost)}>
-      <input placeholder="Title..." {...register("title")} />
-      <p>{errors.title?.message}</p>
-      <textarea placeholder="Description..." {...register("description")} />
-      <p>{errors.description?.message}</p>
-      <input type="submit" />
+    <form onSubmit={handleSubmit(onCreatePost)} >
+      <div className="createPostContainer">
+        <input id="createPostTitleInput" placeholder="Title..." {...register("title")} />
+        <p>{errors.title?.message}</p>
+        <textarea id="createPostDescription" placeholder="Description..." {...register("description")} />
+        <p>{errors.description?.message}</p>
+        <input id="createPostSubmitButton" type="submit" />
+      </div>
     </form>
   );
 };
